f6cb3f0b1818cde8e850795b51f4fb042fe248fe
Pavlo Fatula <pavlo.fatula@edvantis.com>
2013-11-07 16:37:26 +0200

    init

---
+++ main_m/src/main.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: pavlo.fatula
+ * Date: 07.11.13
+ * Time: 16:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class main {
+}
